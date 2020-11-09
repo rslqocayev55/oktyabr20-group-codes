a2e9c1f6c05d358b0d2a0ab705a73863446db135
Yusif Alizada <yusifedu@gmail.com>
2020-11-09 13:40:31 +0400

    security role based authentication

--- year2/spring/demo-crud/src/main/java/com/yalizada/democrud/config/SecurityConfig.java
+++ year2/spring/demo-crud/src/main/java/com/yalizada/democrud/config/SecurityConfig.java
@@ -4,14 +4,39 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
 
+        http.csrf().disable()
+                .authorizeRequests()
+               
+            
+                .antMatchers(HttpMethod.GET,"/").hasAnyRole("DELETOR","ADMIN","EDITOR","CREATOR")
+                .antMatchers(HttpMethod.GET,"/delete/**").hasAnyRole("DELETOR","ADMIN")
+                .antMatchers(HttpMethod.GET,"/edit/**").hasAnyRole("EDITOR","ADMIN")
+                .antMatchers(HttpMethod.POST,"/adduser").hasAnyRole("CREATOR","ADMIN")
+                .antMatchers(HttpMethod.GET,"/signup").hasAnyRole("CREATOR","ADMIN")
+                
+                
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+             
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                 ;
+    }
 	
 
 	@Autowired
