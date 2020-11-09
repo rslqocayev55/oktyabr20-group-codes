aeea23482a236ec9d5177a5d5306dea3f6af08a0
Yusif Alizada <yusifedu@gmail.com>
2020-11-09 15:26:23 +0400

    file upload step

--- year2/spring/demo-crud/src/main/java/com/yalizada/democrud/DemoCrudApplication.java
+++ year2/spring/demo-crud/src/main/java/com/yalizada/democrud/DemoCrudApplication.java
@@ -1,13 +1,26 @@
 package com.yalizada.democrud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.yalizada.democrud.file.StorageProperties;
+import com.yalizada.democrud.file.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class DemoCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoCrudApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
---
+++ year2/spring/demo-crud/src/main/java/com/yalizada/democrud/file/StorageProperties.java
@@ -0,0 +1,17 @@
+package com.yalizada.democrud.file;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+    private String location = "upload-dir-users";
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+    
+}
